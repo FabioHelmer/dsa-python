75e86929a37d45bb091a9a6f27aeaff80c31ce89
fabio.kuhn <fabio.kuhn@el.com.br>
2020-07-01 13:01:44 -0300

    alterando dados do labs

--- lab/ExtracaoDadosTwitter/ConexaoTwitter.py
+++ lab/ExtracaoDadosTwitter/ConexaoTwitter.py
@@ -7,13 +7,13 @@ import json
 import tweepy as tw
 
 # consumer key
-consumer_key = "yEx7mCjjgM9Z9hez4ZP1kL5xV"
+consumer_key = ""
 # consumer secrety
-consumer_secrety = "cno7DN2EHvyc7ZKtg6a91yRnAh6oqzvYTiRF2my4SKvRgoUdlr"
+consumer_secrety = ""
 # access token
-access_token = "1267792852551118851-SbFDhj0Do77ZCzIv8TMm5hG158BJ57"
+access_token = ""
 # access token secrety
-access_token_secrety = "kSp2fVXSMS9N49byhaetSGydCF6T35uGxI949ob39gz0f"
+access_token_secrety = ""
 
 
 # auth
